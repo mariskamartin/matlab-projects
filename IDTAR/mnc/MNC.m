03ae85de0d5327461d0c3918747ef111ca740feb
unknown <st22221@KE36.upce.cz>
2015-06-04 09:14:49 +0200

    uprava skriptu MNC na MIMO

--- IDTAR/mnc/MNC.m
+++ IDTAR/mnc/MNC.m
@@ -2,6 +2,7 @@ function [A, B]=MNC(y,u,na,nb)
 %Offline metoda nejmensich ctvercu. zdroj: skriptum INPRE, Dušek F.
 %funkce vzpocita parametry a.., b.. pomoci metody nejmensich ctvercu:
 %          Y = D * P,
+%          y(k) = B(1)*u(k-1)+B(2)*u(k-2)-A(1)*y(k-1)-A(2)*y(k-2) 
 % P ... vektor parametru
 % D ... datova matice
 %kde P = [a(1); a(2); ...; a(na); b(1); b(2); ...; b(nb)]
@@ -23,8 +24,6 @@ for j=na+1:na+nb,
     end
 end
 Y=y(n+1:end)';
-h1=inv(D'*D);
-h2=h1*D';
-P=h2*Y; % P = inv(D'D)*D'*Y - P je odhad stredni hodnoty parametru
-A=[1; P(1:na)]';
-B=[0; P(na+1:end)]';
+P=inv(D'*D)*D'*Y; % P = inv(D'D)*D'*Y - P je odhad stredni hodnoty parametru
+A=P(1:na)';
+B=P(na+1:end)';
--- IDTAR/mnc/ds_2order_MNC.m
+++ IDTAR/mnc/ds_2order_MNC.m
@@ -3,38 +3,40 @@
 % estimace stavu (adaptivni kalman)
 %
 clear all;
-Ts = 0.01; %time stamp / vzorkovaci perioda
-numOfValues = 1000; %number of values in one step 
+Ts = 0.1; %time stamp / vzorkovaci perioda
+numOfValues = 500; %number of values in one step 
 %vytvoreni systemu 2. radu
-sysTF = tf(1,[1 1 1]); D=c2d(sysTF,Ts); %to discreet representation
+sysTF = tf([1 1],[5 1 1]); D=c2d(sysTF,Ts); %to discreet representation
 [A,B,C,D]=ssdata(D); %to state space model
+radSoustavy = length(B);
 
-u = ones(1,numOfValues*3); %input data for step
-w = [0*ones(1,numOfValues) 1*ones(1,numOfValues) 0.5*ones(1,numOfValues)]; %reference variable
+u = ones(1,numOfValues*4); %input data for step
+w = [0.2*ones(1,numOfValues) 1*ones(1,numOfValues) 0.5*ones(1,numOfValues) 0*ones(1,numOfValues)]; %reference variable
 t = 0:Ts:length(u)*Ts; %time data
-x = [0; 0]; %initial state - vertical
+x = zeros(radSoustavy, 1); %initial state - vertical
+yMnc = zeros(1,3); %start rekonstrukce pomoci mnc
 
 %inicializace
 for k=1:length(u)
-  y(k) = C*x + D*u(k); %out with measurement noise
+  y(k) = C*x + D*u(k) +randn()*0.0001; %out with measurement noise
   % time update
   u(k) = w(k);
   x = A*x + B*u(k);
 end
 
 %identifikace parametru pomoci MNC
-[A, B]=MNC(y(3:end),u(3:end),2,2);
-
-for k=3:length(u)
-    gy(k) = B(1)*u(k-1)+B(2)*u(k-2)-A(1)*y(k-1)-A(2)*y(k-2);
+[A, B]=MNC(y(3:end),u(3:end),radSoustavy,radSoustavy);
+for k=(radSoustavy+1):length(u)
+    yMnc(k) = B*u(k-1:-1:k-radSoustavy)'-A*yMnc(k-1:-1:k-radSoustavy)';
+%     yMnc(k) =B(1)*u(k-1)+B(2)*u(k-2)-A(1)*yMnc(k-1)-A(2)*yMnc(k-2);
 end
 
 
 clf;
 hold on; 
-plot(t(1:end-1), gy, 'k');
-stairs(t(1:end-1),w,'-m');
+stairs(t(1:end-1),u,'-b');
 plot(t(1:end-1),y,'-r');
-legend('mnc','w','y');
+plot(t(1:end-1), yMnc, 'k');
+legend('u','y','mnc');
 
 
