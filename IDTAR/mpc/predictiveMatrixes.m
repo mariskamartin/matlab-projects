2d2c1b82f6147d1251cde1f9937b5b77ce15a448
unknown <st22221@KE36.upce.cz>
2015-06-05 14:36:05 +0200

    added function for create predictive matrixies

---
+++ IDTAR/mpc/predictiveMatrixes.m
@@ -0,0 +1,21 @@
+function [Syx, Syu, Sxx, Sxu] = predictiveMatrixes(A,B,C,N)
+%PREDICTIVEMATRIXES creates predictive matrixes 
+% A,B,C ... system characteristics
+% N     ... number of see-ahead ()
+
+Sxx = A^N;
+for n = 1:N
+    Sxu(:,n) = A^(N-n)*B; 
+    Syx(n,:) = C*A^n*B; 
+end
+
+for n=0:N-1
+    for i=1:N
+        if (i+n <= N) 
+            Syu(i+n,i) = C*A^n*B; 
+        end
+    end
+end
+
+
+
